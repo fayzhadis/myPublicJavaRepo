709ccabda6d15ddc90d67223024fb175dde2da4f
Hadis Fayz <fayzhadis@gmail.com>
2024-04-02 06:12:44 +0200

    first commit

---
+++ MyFirstGitJavaClass.java
@@ -0,0 +1,6 @@
+public class MyFirstGitJavaClass {
+    public static void main(String[] args) {
+        System.out.println("Does my Git work?");
+        System.out.printf("This is my next Git statement");
+    }
+}
